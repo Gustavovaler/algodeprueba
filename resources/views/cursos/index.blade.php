b2dbab4a83cee636d9ca1db5bbaff07b17968872
claudioquin <bmx347@gmail.com>
2020-11-23 21:17:32 -0300

    solucionado los iconos con js

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="{{asset('css/all.css')}}">
+
 <link rel="stylesheet" href="{{asset('css/cursos_index.css')}}">
+<script src="{{asset('js/js/all.js')}}" defer></script>
 
 <img src="{{asset('img/cursos_portada.jpg')}}" alt="imagen de portada" class="portada">
 
